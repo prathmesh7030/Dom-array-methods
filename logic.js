0f983e7a2f6d41eb91ab091666d09b19c27c329d
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-18 22:55:26 +0530

    Calculate the total wealth

--- logic.js
+++ logic.js
@@ -49,3 +49,14 @@ function showMillionaires() {
 
   updateDOM();
 }
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
