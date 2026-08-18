4bbf2d423ca34937a2eeb21444596f04e213af8c
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-18 22:59:55 +0530

    Add new obj & Update DOM

--- logic.js
+++ logic.js
@@ -60,3 +60,25 @@ function calculateWealth() {
   )}</strong></h3>`;
   main.appendChild(wealthEl);
 }
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
