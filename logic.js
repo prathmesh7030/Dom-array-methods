ac74d3abd22b5131828a91c83679b460af598796
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-18 22:51:26 +0530

    Fetch random user and add money

--- logic.js
+++ logic.js
@@ -10,3 +10,18 @@ let data = [];
 getRandomUser();
 getRandomUser();
 getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
