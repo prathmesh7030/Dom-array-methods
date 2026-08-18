631fd6adaca074de78dd952fdf79af4c2eb94af1
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-18 22:54:21 +0530

    Sort & Filter data'

--- logic.js
+++ logic.js
@@ -34,3 +34,18 @@ function doubleMoney() {
 
   updateDOM();
 }
+
+// Sort users by richest
+function sortByRichest() {
+  console.log(123);
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+
+  updateDOM();
+}
